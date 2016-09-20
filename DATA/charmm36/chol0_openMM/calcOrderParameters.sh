12a2cbc8ab0a1a5bf7fdc8309018af14cb80b414
Josef Melcr <melcr@marge.uochb.cas.cz>
2016-09-20 15:01:00 +0200

    added data for CHARMM36 pure POPC membrane simulated with Gmx5.1 and oMM7, yielding same results

---
+++ DATA/charmm36/chol0_openMM/calcOrderParameters.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+tprname=step7_11.tpr
+trajname=${tprname%.tpr}_2.dcd
+trajgroname=${trajname%.dcd}_run.gro
+gromacs_folder_prefix="../gromacs"
+sn1outname=OrderParamSN1_sim2.dat
+sn2outname=OrderParamSN2_sim2.dat
+
+# workaround with tpr file from gromacs simulation (which should match exactly)
+echo System | gmx trjconv -f $trajname -o $trajgroname -s $gromacs_folder_prefix/$tprname -pbc res   #-b 100000 
+
+
+# sn-1
+for((  j = 3 ;  j <= 16;  j=j+1  ))
+do
+Cname=$(grep M_G1C"$j"_M ../../MAPPING/mappingPOPCcharmm.txt | awk '{printf "%5s\n",$2}')
+H1name=$(grep M_G1C"$j"H1_M ../../MAPPING/mappingPOPCcharmm.txt | awk '{printf "%5s\n",$2}')
+H2name=$(grep M_G1C"$j"H2_M ../../MAPPING/mappingPOPCcharmm.txt | awk '{printf "%5s\n",$2}')
+H1op=$(awk -v Cname="$Cname" -v Hname="$H1name" -f ../../scripts/gro_OP.awk $trajgroname)
+H2op=$(awk -v Cname="$Cname" -v Hname="$H2name" -f ../../scripts/gro_OP.awk $trajgroname)
+echo $j $H1op $H2op >> $sn1outname 
+done
+
+# sn-2
+for((  j = 3 ;  j <= 18;  j=j+1  ))
+do
+Cname=$(grep M_G2C"$j"_M ../../MAPPING/mappingPOPCcharmm.txt | awk '{printf "%5s\n",$2}')
+H1name=$(grep M_G2C"$j"H1_M ../../MAPPING/mappingPOPCcharmm.txt | awk '{printf "%5s\n",$2}')
+H2name=$(grep M_G2C"$j"H2_M ../../MAPPING/mappingPOPCcharmm.txt | awk '{printf "%5s\n",$2}')
+H1op=$(awk -v Cname="$Cname" -v Hname="$H1name" -f ../../scripts/gro_OP.awk $trajgroname)
+H2op=$(awk -v Cname="$Cname" -v Hname="$H2name" -f ../../scripts/gro_OP.awk $trajgroname)
+echo $j $H1op $H2op >> $sn2outname
+done
